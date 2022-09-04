b527ea62fd02057985bb522a84a0cf4466550017
omarym22 <omar24xv@gmail.com>
2022-09-04 15:49:57 +0200

    task01:sprints.MyFunc

--- sprint.py
+++ sprint.py
@@ -1,39 +1,45 @@
-# creating an empty list
-lst = []
-# number of elements as input
-n = int(input("Enter number of elements : "))
-print("enter the numbers")
-# iterating till the range
-for i in range(0, n):
-    ele = int(input())
+def MyFunc():
+    lst = []
+    # number of elements as input
+    n = int(input("Enter number of elements : "))
+    print("enter the numbers")
 
-    lst.append(ele)  # adding the element
+    for i in range(0, n):
+        ele = int(input())
 
-print(lst)
+        lst.append(ele)  # adding the element
+
+    print(lst)
+
+    if ele % 2 == 0:
+
+        def averageOfList():
+            sumOfNumbers = 0
+            for t in lst:
+                sumOfNumbers = sumOfNumbers + t
+
+            avg = sumOfNumbers / n
+            print("the mean of even element is ")
+            print(avg)
+
+        averageOfList()
+    else:
+        print("wordLength is odd")
+
+    lst = []
+
+    # number of elements as input
+    n = int(input("Enter number of flout elements : "))
+    print("enter the flout numbers")
+    # iterating till the range
+    for i in range(0, n):
+        ele = (input())
+
+        lst.append(ele)  # adding the element
+
+    print(lst)
+    mx = max(lst)
+    print("the maximum flout number is " + mx)
 
 
-def averageOfList():
-    sumOfNumbers = 0
-    for t in lst:
-        sumOfNumbers = sumOfNumbers + t
-
-    avg = sumOfNumbers / n
-    print(avg)
-
-
-averageOfList()
-
-lst = []
-
-# number of elements as input
-n = int(input("Enter number of flout elements : "))
-print("enter the flout numbers")
-# iterating till the range
-for i in range(0, n):
-    ele = (input())
-
-    lst.append(ele)  # adding the element
-
-print(lst)
-mx = max(lst)
-print("the maximum flout number is " + mx)
+MyFunc()
